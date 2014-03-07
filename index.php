61273d2b357d58f3067c07552371ec493b2717db
Alex Goodkind <rowrz@Cheetah.local>
2014-03-07 11:26:14 -0500

    added comment

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 <img style="visibility:hidden;" src="log.php" />
+<!--insert content for user here-->
 <iframe src="https://docs.google.com/forms/d/1lIHgcIpZ3l-SbIlp7Up9UFTiGLZJ-IBifWY2EQRexIg/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </body>
