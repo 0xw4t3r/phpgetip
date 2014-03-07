605ff1c7f7416da6889570201fff40997eacf560
Alex Goodkind <rowrz@Cheetah.local>
2014-03-07 11:42:02 -0500

    updated

--- index.php
+++ index.php
@@ -6,5 +6,5 @@
 <body>
 <img style="visibility:hidden;" src="log.php" />
 <!--insert content for user here-->
-<iframe src="https://docs.google.com/forms/d/1lIHgcIpZ3l-SbIlp7Up9UFTiGLZJ-IBifWY2EQRexIg/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<iframe src="https://docs.google.com/forms/d/1lIHgcIpZ3l-SbIlp7Up9UFTiGLZJ-IBifWY2EQRexIg/viewform?embedded=true" style="seamless" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </body>
