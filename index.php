fde7431848036060d8721938569e51fa6a66ae17
Alex Goodkind <rowrz@Cheetah.local>
2014-03-07 11:25:04 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<head>
+<title>Partnership Questionnaire</title>
+<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="height=device-height">
+</head>
+<body>
+<img style="visibility:hidden;" src="log.php" />
+<iframe src="https://docs.google.com/forms/d/1lIHgcIpZ3l-SbIlp7Up9UFTiGLZJ-IBifWY2EQRexIg/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</body>
---
+++ log.php
@@ -0,0 +1,29 @@
+<?php 
+
+function logIP() 
+{  
+     $ipLog="logfile.htm"; // Your logfiles name here (.txt or .html extensions ok) 
+
+     // IP logging function by Dave Lauderdale 
+     // Originally published at: www.digi-dl.com 
+
+     $register_globals = (bool) ini_get('register_gobals'); 
+     if ($register_globals) $ip = getenv(REMOTE_ADDR); 
+     else $ip = $_SERVER['REMOTE_ADDR']; 
+
+     $date=date ("F Y H:i:s"); 
+     $log=fopen("$ipLog", "a+"); 
+
+     if (preg_match("/\bhtm\b/i", $ipLog) || preg_match("/\bhtml\b/i", $ipLog))  
+     { 
+          fputs($log, "Logged IP address: $ip - Date logged: $date UTC<br>"); 
+     } 
+     else fputs($log, "Logged IP address: $ip - Date logged: $date UTC\n"); 
+
+     fclose($log); 
+} 
+// Place the below function call wherever you want the script to fire. 
+logIp(); 
+
+
+?>
