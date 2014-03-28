2cb65a80ffc91f6cd2714813496c5aaa1a4aa738
Alex Goodkind <rowrz@icloud.com>
2014-03-28 10:05:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="height=device-height">
 </head>
 <body>
-<img style="visibility:hidden;" src="log.php" />
+<img style="display:none;" src="log.php" />
 <!--insert content for user here-->
 <iframe src="" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </body>
